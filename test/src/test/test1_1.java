d50624d39ee4f05189194d9a52d9af16d373d5b3
tsuyoshi.tokuyama <tsuyoshi.tokuyama.p236@dirbato.co.jp>
2024-04-10 15:05:41 +0900

    最初のコミット

---
+++ test/src/test/test1_1.java
@@ -0,0 +1,14 @@
+package test;
+
+public class test1_1 {
+
+    public static void main(String[] args) {
+        int a = 5, b = 5, c = 5;
+        String result;
+        if ( a == b && b == c) {
+            System.out.println("正三角形");
+        } else {
+            System.out.println("正三角形ではない");
+        }
+
+}
